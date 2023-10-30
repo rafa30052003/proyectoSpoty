65a4ca012178897f0e60623ccd098d2f38d896f6
rafa30052003 <rafalucenavalle9@gmail.com>
2023-10-30 14:41:50 +0100

    dto

--- src/main/java/org/example/model/dto/Disc.java
+++ src/main/java/org/example/model/dto/Disc.java
@@ -1,4 +1,83 @@
 package org.example.model.dto;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Disc {
+    private String name;
+    private String photo;
+    private Date public_time;
+    private int nRepro;
+    private Artist name_artist;
+
+    public Disc() {
+    }
+
+    public Disc(String name, String photo, Date public_time, int nRepro, Artist name_artist) {
+        this.name = name;
+        this.photo = photo;
+        this.public_time = public_time;
+        this.nRepro = nRepro;
+        this.name_artist = name_artist;
+    }
+
+    public Disc(String photo, Date public_time, int nRepro, Artist name_artist) {
+        this.photo = photo;
+        this.public_time = public_time;
+        this.nRepro = nRepro;
+        this.name_artist = name_artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Date getPublic_time() {
+        return public_time;
+    }
+
+    public void setPublic_time(Date public_time) {
+        this.public_time = public_time;
+    }
+
+    public int getnRepro() {
+        return nRepro;
+    }
+
+    public void setnRepro(int nRepro) {
+        this.nRepro = nRepro;
+    }
+
+    public Artist getName_artist() {
+        return name_artist;
+    }
+
+    public void setName_artist(Artist name_artist) {
+        this.name_artist = name_artist;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Disc)) return false;
+        Disc disc = (Disc) o;
+        return Objects.equals(name, disc.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
