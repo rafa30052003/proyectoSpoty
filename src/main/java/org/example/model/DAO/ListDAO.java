8dca405922565ddb71cb6053771ec6a42387365f
javierg277 <javiergonzalezaguilar4@gmail.com>
2023-11-08 19:39:07 +0100

    Listdaofuncional

--- src/main/java/org/example/model/DAO/ListDAO.java
+++ src/main/java/org/example/model/DAO/ListDAO.java
@@ -1,14 +1,14 @@
 package org.example.model.DAO;
 
-import java.sql.*;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import org.example.conexion.Connect;
 import org.example.interfaceDAO.iDAO;
 import javafx.collections.FXCollections;
-import org.example.model.dto.List;
 import javafx.collections.ObservableList;
+import org.example.conexion.Connect;
+import org.example.conexion.ConnectionData;
+import org.example.model.dto.List;
+import java.sql.*;
+import java.util.ArrayList;
+
 
 public class ListDAO extends List implements iDAO<List, Integer> {
 
@@ -27,7 +27,7 @@ public class ListDAO extends List implements iDAO<List, Integer> {
 
     public static void conectar() {
         try {
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/eventos_programacion", "root", "");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/spotifyproject", "root", "");
         } catch (SQLException var1) {
             var1.printStackTrace();
         }
@@ -47,23 +47,23 @@ public class ListDAO extends List implements iDAO<List, Integer> {
     public static void addList(List b) throws SQLException {
         conectar();
         PreparedStatement stat = null;
-        stat = con.prepareStatement("insert into list(name_user,name_list,description) values(?,?,?)");
-        stat.setString(1, b.getName_list());
-        stat.setString(2, String.valueOf(b.getName_user()));
-        stat.setString(2, b.getDescription());
+        stat = con.prepareStatement("insert into list(id,name_user,name_list,description) values(null,?,?,?);");
+        stat.setString(2, b.getName_list());
+        stat.setString(1, String.valueOf(b.getName_user()));
+        stat.setString(3, b.getDescription());
         stat.executeUpdate();
     }
-    public static ObservableList<List> getAllList() throws SQLException {
+    public static ArrayList<List> getAllList() throws SQLException {
         conectar();
-        ObservableList<List> obs = FXCollections.observableArrayList();
+        ArrayList<List> obs = new ArrayList<>();
         Statement stat = con.createStatement();
-        ResultSet rs = stat.executeQuery("SELECT name_list,name_user,descripcion,id from LIST");
+        ResultSet rs = stat.executeQuery("SELECT name_list,name_user,description,id from LIST");
 
         while(rs.next()) {
             new List();
             String nameL = rs.getString("name_list");
             String nameU = rs.getString("name_user");
-            String  Ldescripition = rs.getString("descripcion");
+            String  Ldescripition = rs.getString("description");
             int Lid = Integer.parseInt(rs.getString("id"));
             List e = new List(Lid,nameL,Ldescripition,nameU);
             obs.add(e);
@@ -75,13 +75,13 @@ public class ListDAO extends List implements iDAO<List, Integer> {
     public static void Modlist(List b) throws SQLException {
         conectar();
         PreparedStatement stat = null;
-        stat = con.prepareStatement("UPDATE LIST" +
-                "SET name_list = value(?),\n" +
-                "    description = value(?)\n" +
-                "WHERE id = value(?);");
+        stat = con.prepareStatement("UPDATE LIST " +
+                "SET List.name_list = ?, \n" +
+                "    List.description = ?\n " +
+                "WHERE list.id = ?;");
         stat.setString(1, b.getName_list());
         stat.setString(2, b.getDescription());
-        stat.setInt(2, b.getId());
+        stat.setInt(3, b.getId());
         stat.executeUpdate();
     }
 
@@ -103,5 +103,13 @@ public class ListDAO extends List implements iDAO<List, Integer> {
     @Override
     public void delete(List entity) throws SQLException {
 
+    }
+
+    public static void deleteById(int id) throws SQLException {
+        conectar();
+        PreparedStatement stat = null;
+        stat = con.prepareStatement("DELETE FROM list WHERE list.id = ?");
+        stat.setInt(1, id);
+        stat.executeUpdate();
     }
 }
